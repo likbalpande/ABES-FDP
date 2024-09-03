0429f7fa4ca5d1b08449d580ea4be316ba8c0570
Likhilesh <likbalpande@example.com>
2024-09-03 13:01:33 +0530

    more from first half

--- Day10_JS_DOM/script.js
+++ Day10_JS_DOM/script.js
@@ -35,15 +35,37 @@
 // console.dir(res[0].style); // --> style is an object
 // res[0].style.backgroundColor = "brown";
 
-// ------------------ different Selector methods
-const res1 = document.getElementsByTagName("h1");
-console.dir(res1);
+// ------------------ different Selector methods (BASIC)
 
-const res2 = document.getElementsByClassName("title");
+// const res1 = document.getElementsByTagName("h1");
+// // --> iterable (HTMLCollection)
+// res1[0].style.backgroundColor = "green";
+// console.dir(res1);
+
+// const res2 = document.getElementsByClassName("title");
+// // --> iterable (HTMLCollection)
+// res2[0].style.backgroundColor = "yellow";
+// console.dir(res2);
+
+// const res3 = document.getElementById("uniq-title");
+// // --> element or null
+// res3.style.backgroundColor = "red";
+// console.dir(res3);
+
+// const res4 = document.getElementById("uniq-title-np");
+// console.dir(res4);
+
+// ------------------ different Selector methods (MORE)
+// const res1 = document.querySelectorAll("h1:first-child");
+// // --> iterable (NodeList)
+// console.log(res1);
+
+const res2 = document.querySelectorAll("div>h1>i");
+// --> iterable (NodeList)
 console.dir(res2);
+res2[0].style.color = "violet";
 
-const res3 = document.getElementById("uniq-title");
+const res3 = document.querySelector("div>h1>i");
+// --> Node
 console.dir(res3);
-
-const res4 = document.getElementById("uniq-title-np");
-console.dir(res4);
+res3.style.textDecoration = "underline";
